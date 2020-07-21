e911b9ab09c9297a289d7b95aac4b14b59ff1d54
nagasudhirpulla <nagasudhirpulla@gmail.com>
2020-07-21 15:23:11 +0530

    comments in for loop

--- day_1/05_for_in_python.py
+++ day_1/05_for_in_python.py
@@ -2,24 +2,28 @@
 Lesson 5 - 'for' statement in python and using it with 'range' function
 '''
 
-for itr in [1,5,8,7,45]:
-    print('executing...')
-    print('x is {0}'.format(itr))
-
-print('execution complete with arrays...\n\n')
-
+# looping for 10 times
 for itr in range(10):
     print('executing...')
     print('x is {0}'.format(itr))
 
 print('execution complete with range(10)...\n\n')
 
+# looping with each element of an array
+for itr in [1,5,8,7,45]:
+    print('executing...')
+    print('x is {0}'.format(itr))
+
+print('execution complete with arrays...\n\n')
+
+# looping on numbers from 4 to 8
 for itr in range(4,9):
     print('executing...')
     print('x is {0}'.format(itr))
 
 print('execution complete with range(4,9)...\n\n')
 
+# looping on numbers from 4 to 11 with step interval of 2
 for itr in range(4,12,2):
     print('executing...')
     print('x is {0}'.format(itr))
--- day_2/04_dicts_in_python.py
+++ day_2/04_dicts_in_python.py
@@ -3,3 +3,15 @@ Day 2 - Lesson 4
 Dictionaries in python
 '''
 
+x = {
+    'firstName': 'Nagasudhir',
+    'lastname': 'Pulla',
+    'age': 28,
+    'hobbies': ['tv', 'playing', 'youtube'],
+    'metaData': {
+        'proficiency': 'level 1',
+        'designation': 'Deputy Manager',
+        'department': 'IT',
+        'languages': ['C#', 'Javascript', 'HTML', 'CSS', 'typescript', 'python']
+    }
+}
